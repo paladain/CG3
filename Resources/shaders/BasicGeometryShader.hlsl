79ebcae543c8a419a1d442e91f7566e6976a4371
OnoMasachika <k021g1121@g.neec.ac.jp>
2022-11-11 09:25:20 +0900

    02_04 まで終わり

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,27 +1,81 @@
 #include "BasicShaderHeader.hlsli"
 
-[maxvertexcount(6)]
-void main(
-	triangle VSOutput input[3] : SV_POSITION, 
-	inout TriangleStream< GSOutput > output
-)
-{
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv;
-		output.Append(element);
-	}
-	output.RestartStrip();
+//[maxvertexcount(6)]
+//void main(
+//	triangle VSOutput input[3] : SV_POSITION, 
+//	inout TriangleStream< GSOutput > output
+//)
+//{
+//	for (uint i = 0; i < 3; i++)
+//	{
+//		GSOutput element;
+//		element.svpos = input[i].svpos;
+//		element.normal = input[i].normal;
+//		element.uv = input[i].uv;
+//		output.Append(element);
+//	}
+//	output.RestartStrip();
+//
+//	for (uint i = 0; i < 3; i++)
+//	{
+//		GSOutput element;
+//		element.svpos = input[i].svpos + float4(20.0f, 0.0f, 0.0f, 0.0f);
+//		element.normal = input[i].normal;
+//		element.uv = input[i].uv * 2.0f;
+//		output.Append(element);
+//	}
+//}
 
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos + float4(20.0f, 0.0f, 0.0f, 0.0f);
-		element.normal = input[i].normal;
-		element.uv = input[i].uv * 2.0f;
+//[maxvertexcount(3)]
+//void main(
+//	point VSOutput input[1] : SV_POSITION,
+//	inout TriangleStream< GSOutput > output
+//)
+//	
+//	{
+//		GSOutput element;
+//		element.normal = input[0].normal;
+//		element.uv = input[0].uv;
+//
+//		element.svpos = input[0].svpos;
+//		output.Append(element);
+//
+//		element.svpos = input[0].svpos + float4(10, 10, 0, 0);
+//		output.Append(element);
+//
+//		element.svpos = input[0].svpos + float4(10, 0, 0, 0);
+//		output.Append(element);
+//	}
+
+static const uint vnum = 4;
+
+static const float4 offset_array[vnum] =
+{
+	float4(-0.5f, -0.5f,0,0),
+	float4(-0.5f, +0.5f,0,0),
+	float4(+0.5f, -0.5f,0,0),
+	float4(+0.5f, +0.5f,0,0)
+};
+
+static const float2 uv_array[vnum] =
+{
+	float2(0, 1),
+	float2(0, 0),
+	float2(1, 1),
+	float2(1, 0)
+};
+
+[maxvertexcount(vnum)]
+void main(
+	point VSOutput input[1] :SV_POSITION,
+	inout TriangleStream < GSOutput > output)
+{
+	GSOutput element;
+	for (uint i = 0; i < vnum; i++) {
+		element.svpos = input[0].pos + offset_array[i];
+		element.svpos = mul(mat, element.svpos);
+		/*element.uv = float2(0.5f, 0.5f);*/
+		element.uv = uv_array[i];
 		output.Append(element);
 	}
 }
--- Resources/shaders/BasicVertexShader.hlsl
+++ Resources/shaders/BasicVertexShader.hlsl
@@ -1,10 +1,11 @@
 #include "BasicShaderHeader.hlsli"
 
-VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
+VSOutput main(float4 pos : POSITION/*, float3 normal : NORMAL, float2 uv : TEXCOORD*/)
 {
 	VSOutput output; // ピクセルシェーダーに渡す値
-	output.svpos = mul(mat, pos);
+	/*output.svpos = mul(mat, pos);
 	output.normal = normal;
-	output.uv = uv;
+	output.uv = uv;*/
+	output.pos = pos;
 	return output;
 }
